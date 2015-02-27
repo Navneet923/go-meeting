db2208a8154b1bc23c722731291f341dff81b231
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-02-27 11:31:11 -0800

    Very small gorilla mux based router + server

--- server.go
+++ server.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-    "fmt"
-    "regexp"
-    "net/http"
-) 
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
-// Define HTTP route structure for the server
-type Route struct {
-    pattern *regexp.Regexp
-    method  string
-    handler http.Handler
+// Main entry point
+func main() {
+	router := mux.NewRouter()
+
+	// Define error'd route handler
+	router.NotFoundHandler = http.HandlerFunc(Render404)
+
+	// Define various application routes here
+	router.HandleFunc("/", RenderIndex).Methods("GET")
+
+	// Use the above router for all routes
+	http.Handle("/", router)
+
+	fmt.Printf("Server up and listening...")
+	http.ListenAndServe("0.0.0.0:3000", nil)
 }
 
+// Render 4040 page
+func Render404(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("Hmm looks like we 404'd trying to find: " + request.URL.Path))
+}
 
-
-
+// Render Home page
+func RenderIndex(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("Hello world!"))
+}
