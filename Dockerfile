9f0932a20fae11e9291a32194f465cc41a01d4b2
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-03-01 20:32:40 -0800

    Update server code, improved docker file since we wish to run our server from a specific directory

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,19 @@
 # Dockerfile for the `go-meeting` service
-FROM golang:onbuild
+FROM golang
 MAINTAINER shaba abhiram
+
+# Add the cwd to its corresponding location in the docker container
+ADD . /go/src/github.com/sabhiram/go-meeting
+
+# Install deps
+RUN go get github.com/gorilla/mux
+
+# Build the application
+RUN go install github.com/sabhiram/go-meeting/go-meeting
+
+# Setup the entry-point
+WORKDIR /go/src/github.com/sabhiram/go-meeting
+ENTRYPOINT /go/bin/go-meeting
 
 # Expose any needed ports
 EXPOSE 3000
--- server/server.go
+++ server/server.go
@@ -1,36 +1,36 @@
 package main 
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
-	"net/http"
+    "fmt"
+    "github.com/gorilla/mux"
+    "net/http"
 )
 
 // Setup the routes for the server and any static assets
 func SetupServer() {
-	router := mux.NewRouter()
+    router := mux.NewRouter()
 
-	// Define error'd route handler
-	router.NotFoundHandler = http.HandlerFunc(Render404)
+    // Define error'd route handler
+    router.NotFoundHandler = http.HandlerFunc(Render404)
 
-	// Define various application routes here
-	router.HandleFunc("/", RenderIndex).Methods("GET")
+    // Define various application routes here
+    router.HandleFunc("/", RenderIndex).Methods("GET")
+        
+    // Serve any app related static content
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("go-meeting/static"))))
 
-	// Use the above router for all routes
-	http.Handle("/", router)
-
-        // Serve any app related static content
-        http.Handle("/static/", http.FileServer(http.Dir("static")))
+    // Use the above router for all routes
+    http.Handle("/", router)
 }
 
 // Render 404 page
 func Render404(response http.ResponseWriter, request *http.Request) {
-	response.Write([]byte("Hmm looks like we 404'd trying to find: " + request.URL.Path))
+    response.Write([]byte("Hmm looks like we 404'd trying to find: " + request.URL.Path))
 }
 
 // Render Home page
 func RenderIndex(response http.ResponseWriter, request *http.Request) {
-	response.Write([]byte("Hello world!"))
+    response.Write([]byte("Hello world!"))
 }
 
 // Main entry point
