83f7f2b99ead079ffce472158bfad1ce747a95d4
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-03-02 14:06:28 -0800

    Wired up server and external client resources

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
     "github.com/gorilla/mux"
     "net/http"
+    "fmt"
 )
 
 // Setup the routes for the server and any static assets
@@ -13,10 +14,10 @@ func SetupRoutes() {
     router.NotFoundHandler = http.HandlerFunc(Render404)
 
     // Define various application routes here
-    router.HandleFunc("/", RenderIndex).Methods("GET")
+    router.HandleFunc("/api/add/{a:[0-9]+}/{b:[0-9]+}", AddHandler).Methods("GET")
         
     // Serve any app related static content
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("go-meeting/static"))))
+    router.PathPrefix("/").Handler(http.FileServer(http.Dir("server/static")))
 
     // Use the above router for all routes
     http.Handle("/", router)
@@ -27,8 +28,11 @@ func Render404(response http.ResponseWriter, request *http.Request) {
     response.Write([]byte("Hmm looks like we 404'd trying to find: " + request.URL.Path))
 }
 
-// Render Home page
-func RenderIndex(response http.ResponseWriter, request *http.Request) {
-    response.Write([]byte("Hello world!"))
+// Add Handler (POC entrypoint - just adds two numbers)
+func AddHandler(response http.ResponseWriter, request *http.Request) {
+    fmt.Printf("Add handler invoked: %v\n", mux.Vars(request))
+    vars := mux.Vars(request)
+    a, b := vars["a"], vars["b"]
+    response.Write([]byte(a + b))
 }
 
