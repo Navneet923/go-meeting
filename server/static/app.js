9e71b04a6a145dd66088a6187d395c8509e84526
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-03-04 15:39:15 -0800

    Move angular-routes -> angularjs-routes, add basic routing

--- server/static/app.js
+++ server/static/app.js
@@ -4,7 +4,27 @@
  *  Define the app and inject any modules we wish to
  *  refer to.
 ***/
-var app = angular.module("MeetingMinutes", []);
+var app = angular.module("MeetingMinutes", [
+    "ngRoute"
+]);
+
+/***
+ * Define the routes which the application should support
+***/
+app.config(["$routeProvider",
+
+    function($routeProvider) {
+        $routeProvider.
+            when("/meetings", {
+                templateUrl: "partials/meeting-list.html",
+                controller:  "MeetingListController"
+            }).when("/meetings/:meetingId", {
+                templateUrl: "partials/meeting.html",
+                controller:  "MeetingController"
+            }).otherwise({ redirectTo: "/meetings" });
+    }]
+
+);
 
 /******************************************************************************\
 Function:
@@ -33,6 +53,47 @@ app.controller("AppController", ["$scope", "$location", "$timeout",
     }]
 
 );
+
+/******************************************************************************\
+Function:
+    MeetingListController 
+
+Dependencies:
+    ...
+
+Description:
+    Application controller responsible for querying and displaying meetings
+\******************************************************************************/
+app.controller("MeetingListController", ["$scope", "$location", "$timeout",
+
+    function($scope, $location, $timeout) {
+        console.log("Meeting List Controller initialized");
+    }]
+
+);
+
+
+/******************************************************************************\
+Function:
+    MeetingController 
+
+Dependencies:
+    ...
+
+Description:
+    Application controller responsible for rendering a single meeting 
+\******************************************************************************/
+app.controller("MeetingController", ["$scope", "$location", "$timeout",
+
+    function($scope, $location, $timeout) {
+        console.log("Meeting Controller initialized");
+        $scope.meeting = {
+            name: "Placeholder Meeting"
+        }
+    }]
+
+);
+
 
 /*****************************************************************************\
     Service to hit the server
