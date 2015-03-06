486ac89eabc73792b59d3d669c2098e58d1f8c3b
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-03-05 21:37:11 -0800

    Go fmt, lint etc

--- server/server.go
+++ server/server.go
@@ -1,38 +1,38 @@
-package server 
+package server
 
 import (
-    "github.com/gorilla/mux"
-    "net/http"
-    "fmt"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
-// Setup the routes for the server and any static assets
+// SetupRoutes defines the various routes that this server will handle
+// as well as allow for the hosting of static assets
 func SetupRoutes() {
-    router := mux.NewRouter()
+	router := mux.NewRouter()
 
-    // Define error'd route handler
-    router.NotFoundHandler = http.HandlerFunc(Render404)
+	// Define error'd route handler
+	router.NotFoundHandler = http.HandlerFunc(Render404)
 
-    // Define various application routes here
-    router.HandleFunc("/api/add/{a:[0-9]+}/{b:[0-9]+}", AddHandler).Methods("GET")
-        
-    // Serve any app related static content
-    router.PathPrefix("/").Handler(http.FileServer(http.Dir("server/static")))
+	// Define various application routes here
+	router.HandleFunc("/api/add/{a:[0-9]+}/{b:[0-9]+}", AddHandler).Methods("GET")
 
-    // Use the above router for all routes
-    http.Handle("/", router)
+	// Serve any app related static content
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("server/static")))
+
+	// Use the above router for all routes
+	http.Handle("/", router)
 }
 
-// Render 404 page
+// Render404 will render an error page
 func Render404(response http.ResponseWriter, request *http.Request) {
-    response.Write([]byte("Hmm looks like we 404'd trying to find: " + request.URL.Path))
+	response.Write([]byte("Hmm looks like we 404'd trying to find: " + request.URL.Path))
 }
 
-// Add Handler (POC entrypoint - just adds two numbers)
+// AddHandler (POC entrypoint - just adds two numbers)
 func AddHandler(response http.ResponseWriter, request *http.Request) {
-    fmt.Printf("Add handler invoked: %v\n", mux.Vars(request))
-    vars := mux.Vars(request)
-    a, b := vars["a"], vars["b"]
-    response.Write([]byte(a + b))
+	fmt.Printf("Add handler invoked: %v\n", mux.Vars(request))
+	vars := mux.Vars(request)
+	a, b := vars["a"], vars["b"]
+	response.Write([]byte(a + b))
 }
-
