aff2c3a2f8c396e510c7e15cb872e9975d761a98
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2015-03-07 19:01:40 -0800

    Add a couple more route handlers

--- server/server.go
+++ server/server.go
@@ -17,11 +17,16 @@ func SetupRoutes() {
 	// Define error'd route handler
 	router.NotFoundHandler = http.HandlerFunc(Render404)
 
-	// Define various application routes here
-	router.HandleFunc("/api/new_meeting", CreateNewMeeting).Methods("POST")
-
 	// Serve any app related static content
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("server/static")))
+
+	// Define various application routes here
+	// POST Routes:
+	router.HandleFunc("/api/new_meeting", CreateNewMeeting).Methods("POST")
+
+	// GET Routes:
+	router.HanldeFunc("/api/meetings", FetchMeetings).Methods("GET")
+	router.HandleFunc("/api/meeting/{id}", FetchMeetingWithId).Methods("GET")
 
 	// Use the above router for all routes
 	http.Handle("/", router)
@@ -51,12 +56,14 @@ type NewMeeting struct {
 func CreateNewMeeting(response http.ResponseWriter, request *http.Request) {
 	fmt.Printf("POST /api/new_meeting called\n")
 
+	// Read the request body
 	body, err := ioutil.ReadAll(request.Body)
 	if err != nil {
 		panic(err)
 	}
 	defer request.Body.Close()
 
+	// Craft a new meeting, and handle errors
 	var newMeeting NewMeeting
 	if err := json.Unmarshal(body, &newMeeting); err != nil {
 		// Send a mangled request status back
@@ -65,9 +72,22 @@ func CreateNewMeeting(response http.ResponseWriter, request *http.Request) {
 		if err := json.NewEncoder(response).Encode(err); err != nil {
 			panic(err)
 		}
+		return
 	}
 
 	// TODO: Add a newMeeting to the DB here
 	SendHTTPStatusCode(response, http.StatusCreated)
-	fmt.Printf("Got New Meeting request: %V\n", newMeeting)
+}
+
+// FetchMeetings is a GET handler which returns a list of all meetings
+func FetchMeetings(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("TODO: Implement FetchMeetings"))
+}
+
+// FetchMeetingWithId is a GET handler which returns a given
+// meeting (or an error) given an ID
+func FetchMeetingWithId(response http.ResponseWriter, request *http.Request) {
+	// Extract id
+	id := mux.Vars(request)["id"]
+	response.Write([]byte(fmt.Sprintf("%V id recieved.\n", id)))
 }
